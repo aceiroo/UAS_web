ef7ca12b0e43105c5013b15febce0770a0da7bcf
USER <audiedevanugraha@gmail.com>
2020-06-16 23:39:16 +0800

    sec

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php 
 
-$databaseHost = 'localhost';
-$databaseName = 'auddev_project_uas';
-$databaseUsername = 'auddev_project_uas';
-$databasePassword = 'hoho123';
+$databaseHost = 'sql203.epizy.com	
+';
+$databaseName = 'epiz_26027287_project_uas';
+$databaseUsername = 'epiz_26027287';
+$databasePassword = '6UP2YWQu2cqXgpK
+';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 ?>
